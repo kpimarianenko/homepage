0935a8598c95c51784bc2172b89798381bab66e0
Roman Marianenko <kpimarianenko@gmail.com>
2024-12-12 20:14:57 +0200

    pre-commit linting configured

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import importOrder from 'eslint-plugin-import';
+import react from 'eslint-plugin-react';
 import prettier from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
@@ -34,15 +35,19 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier: prettier,
+      prettier,
       import: importOrder,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
       'prettier/prettier': 'error',
+      'no-console': 'warn',
       '@typescript-eslint/no-unused-vars': [
         'warn',
         {
