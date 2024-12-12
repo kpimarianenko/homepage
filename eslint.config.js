e7ce0ccb67a37690b86f71760e3feecde87e8138
Roman Marianenko <kpimarianenko@gmail.com>
2024-12-12 19:01:45 +0200

    eslint configured

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,33 @@
 import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import importOrder from 'eslint-plugin-import';
 import prettier from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
+import { fixupConfigRules } from '@eslint/compat';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  resolvePluginsRelativeTo: __dirname,
+});
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettierConfig],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettierConfig,
+      ...fixupConfigRules(compat.extends('plugin:eslint-comments/recommended')),
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -19,11 +37,74 @@ export default tseslint.config(
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       prettier: prettier,
+      import: importOrder,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'prettier/prettier': 'error',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          ignoreRestSiblings: true,
+          destructuredArrayIgnorePattern: '^_',
+        },
+      ],
+      'arrow-body-style': ['error', 'as-needed'],
+      'func-style': ['error', 'expression'],
+      'padding-line-between-statements': [
+        'error',
+        {
+          blankLine: 'always',
+          prev: ['const', 'let', 'var'],
+          next: '*',
+        },
+        {
+          blankLine: 'any',
+          prev: ['const', 'let', 'var'],
+          next: ['const', 'let', 'var'],
+        },
+      ],
+      'sort-imports': [
+        'warn',
+        {
+          ignoreCase: true,
+          ignoreDeclarationSort: true,
+        },
+      ],
+      'import/order': [
+        'warn',
+        {
+          'newlines-between': 'always',
+          pathGroups: [
+            {
+              pattern: '{react,react-*}',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '~/**',
+              group: 'internal',
+              position: 'before',
+            },
+          ],
+          groups: [
+            ['external', 'builtin'],
+            ['internal', 'parent', 'sibling', 'index'],
+          ],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          distinctGroup: false,
+          pathGroupsExcludedImportTypes: ['internal'],
+        },
+      ],
     },
   }
 );
